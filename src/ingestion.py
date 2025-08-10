5373f73e8372656cdf346279f5d8e12abb1ee21a
yurimarca <yurimarca@gmail.com>
2025-08-10 19:11:03 -0300

    document ingestion to local db

---
+++ src/ingestion.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# vectorstore = Chroma.from_documents(
+#     documents=doc_splits,
+#     collection_name="rag-chroma",
+#     embedding=OpenAIEmbeddings(),
+#     persist_directory="./.chroma",
+# )
+
+retriever = Chroma(
+    collection_name="rag-chroma",
+    persist_directory="./.chroma",
+    embedding_function=OpenAIEmbeddings(),
+).as_retriever()
+
+# Check if the vectorstore is filled
+if len(retriever.get_relevant_documents("test")) == 0:
+    print("Vectorstore is empty, please run the ingestion script first.")
+else:
+    print("Vectorstore is ready with documents.")
+    # You can now use the retriever to query the vectorstore
+    results = retriever.get_relevant_documents("What is prompt engineering?")
+    print(results)
+
