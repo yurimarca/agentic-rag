79fa03b62f6504ab656c054bcfc1d06df4d4ffd2
yurimarca <yurimarca@gmail.com>
2025-08-11 21:03:53 -0300

    add hallucination check

--- self_rag/graph/chains/tests/test_chains.py
+++ self_rag/graph/chains/tests/test_chains.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from graph.chains.generation import generation_chain
+from graph.chains.hallucination_grader import (GradeHallucinations,
+                                               hallucination_grader)
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
 from ingestion import retriever
 
@@ -37,3 +40,26 @@ def test_generation_chain() -> None:
     docs = retriever.invoke(question)
     generation = generation_chain.invoke({"context": docs, "question": question})
     pprint(generation)
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+    assert res.binary_score
+
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {
+            "documents": docs,
+            "generation": "In order to make pizza we need to first start with the dough",
+        }
+    )
+    assert not res.binary_score
